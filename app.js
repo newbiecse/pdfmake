e9e59eca6954900cbe14a1d0c58308eeca509c3e
tannguyen.bk10 <tannguyen.bk10@gmail.com>
2016-07-21 01:30:25 +0700

    use 4 spanCol in table

--- app.js
+++ app.js
@@ -5,27 +5,129 @@ $(function () {
         var tbl = {
             margin: [ 0, 10, 0, 20 ],
             headerRows: 1,
-            widths: [ '50%', '50%'],
+            widths: [ '30%', '20%', '*', '20%'],
 
             body: [
-              [ 'Inspection Date(s):', 'April 7, 2016' ],
-              [ 'Media:', 'CWA 311 SPCC' ],
-              [ 'Regulatory Program(s)', 'Underground Injection Control ­ Brine' ],
-              [ 'Company Name:', 'Calgary Energy LLC' ],
-              [ 'Facility Name:', 'House Lease' ],
-              [ 'Facility Physical Location:', 'SW/4 Section 19­T29N­R9E' ],
-              [ '(city, state, zip code)', 'Chautauqua, KS' ],
-              [ 'Mailing Address:', 'P.O. Box 737' ],
-              [ '(city, state, zip code)', 'Collinsville, OK 74021' ],           
-              [ 'County/Parish:', 'Osage County' ],
-              [ { colSpan: 1, rowSpan: 2, text: 'Facility Contact' }, 'Gary Lewis II (918)798­4511' ],
-              [ '', 'N/A' ],
-              [ 'FRS Number:', 'N/A' ],
-              [ 'Identification/Permit Number:', 'N/A' ],
-              [ 'Media Number:', 'N/A' ],
-              [ 'NAICS:', '21111' ],
-              [ 'SIC:', '1311' ],
-              [ { colSpan: 2, rowSpan: 1, text: 'Personnel participating in the inspection:'}, '' ]
+              [ 
+                { colSpan: 2, text: 'Inspection Date(s):' }, 
+                '',
+                { colSpan: 2, text: 'April 7, 2016' },
+                ''
+              ],
+              [
+                { colSpan: 2, text: 'Media:' },
+                '',
+                { colSpan: 2, text: 'CWA 311 SPCC' },
+                ''
+              ],
+              [ 
+                { colSpan: 2, text: 'Regulatory Program(s)' },
+                '',
+                { colSpan: 2, text: 'Underground Injection Control ­ Brine' },
+                ''
+              ],
+              [ 
+                { colSpan: 2, text: 'Company Name:' },
+                '',
+                { colSpan: 2, text: 'Calgary Energy LLC' },
+                ''
+              ],
+              [
+                { colSpan: 2, text: 'Facility Name:' },
+                '',
+                { colSpan: 2, text: 'House Lease' },
+                ''
+              ],
+              [ 
+                { colSpan: 2, text: 'Facility Physical Location:' },
+                '',
+                { colSpan: 2, text: 'SW/4 Section 19­T29N­R9E' },
+                ''
+              ],
+              [ 
+                { colSpan: 2, text: '(city, state, zip code)' }, 
+                '',
+                { colSpan: 2, text: 'Chautauqua, KS' },
+                ''
+              ],
+              [ 
+                { colSpan: 2, text: 'Mailing Address:' },
+                '',
+                { colSpan: 2, text: 'P.O. Box 737' },
+                ''
+              ],
+              [ 
+                { colSpan: 2, text: '(city, state, zip code)' }, 
+                '',
+                { colSpan: 2, text: 'Collinsville, OK 74021' },
+                ''
+              ],           
+              [
+                { colSpan: 2, text: 'County/Parish:' },
+                '', 
+                { colSpan: 2, text: 'Osage County' },
+                ''
+              ],
+              [ 
+                { colSpan: 2, rowSpan: 2, text: 'Facility Contact' },
+                '',
+                { colSpan: 2, text: 'Gary Lewis II (918)798­4511' },
+                '',
+              ],
+              [ 
+                { colSpan: 2, text: '' }, 
+                '',
+                { colSpan: 2, text: 'N/A' },
+                ''
+              ],
+              [ 
+                { colSpan: 2, text: 'FRS Number:' },
+                '', 
+                { colSpan: 2, text: 'N/A' },
+                ''
+              ],
+              [ 
+                { colSpan: 2, text: 'Identification/Permit Number:' }, 
+                '',
+                { colSpan: 2, text: 'N/A' },
+                ''
+              ],
+              [ 
+                { colSpan: 2, text: 'Media Number:' }, 
+                '',
+                { colSpan: 2, text: 'N/A' },
+                ''
+              ],
+              [ 
+                { colSpan: 2, text: 'NAICS:' }, 
+                '',
+                { colSpan: 2, text: '21111' },
+                ''
+              ],
+              [ 
+                { colSpan: 2, text: 'SIC:' }, 
+                '',
+                { colSpan: 2, text: '1311' },
+                ''
+              ],
+              [ 
+                { colSpan: 4, rowSpan: 1, text: 'Personnel participating in the inspection:'},
+                '',
+                '',
+                '' 
+              ],
+              [ 'Kent Sanborn', '6EN­WR', 'EPA Inspector', '918­557­1615' ],
+              [ 'N/A', 'N/A', 'N/A', '9187984511' ],
+              [ 'N/A', 'N/A', 'N/A', 'N/A' ],
+              [ 'N/A', 'N/A', 'N/A', 'N/A' ],
+              [ 'N/A', 'N/A', 'N/A', 'N/A' ],
+              [ 'N/A', 'N/A', 'N/A', 'N/A' ],
+              [ 'N/A', 'N/A', 'N/A', 'N/A' ],
+              [ 'N/A', 'N/A', 'N/A', 'N/A' ],
+              [ 'N/A', 'N/A', 'N/A', 'N/A' ],
+              [ 'N/A', 'N/A', 'N/A', 'N/A' ],
+              [ 'N/A', 'N/A', 'N/A', 'N/A' ],
+              [ 'N/A', 'N/A', 'N/A', 'N/A' ]
             ]
         }
 
@@ -66,9 +168,9 @@ function htmlToPdfMake(elementID) {
     });
     
     var dd = {
-	content: [
-		fullText
-	]
+  content: [
+    fullText
+  ]
     };
         pdfMake.createPdf(dd).download('optionalName.pdf');
         console.log(dd.content);
