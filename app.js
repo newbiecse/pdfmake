8fac6bbf143277686cb6791ff5159841d3446794
tannguyen.bk10 <tannguyen.bk10@gmail.com>
2016-07-21 23:37:30 +0700

    temporary

--- app.js
+++ app.js
@@ -3,150 +3,155 @@ $(function () {
     $('#btn-print').click(function () {
 
         var tbl = {
-            margin: [ 0, 10, 0, 20 ],
             headerRows: 1,
-            widths: [ '30%', '20%', '*', '20%'],
+            widths: [ '30%', '20%', '20%', '30%'],
 
             body: [
               [ 
-                { colSpan: 2, text: 'Inspection Date(s):' }, 
+                { colSpan: 1, text: 'Inspection Date(s):' }, 
+                { colSpan: 3, text: 'April 7, 2016' },
                 '',
-                { colSpan: 2, text: 'April 7, 2016' },
                 ''
               ],
               [
-                { colSpan: 2, text: 'Media:' },
+                { colSpan: 1, text: 'Media:' },
+                { colSpan: 3, text: 'CWA 311 SPCC' },
                 '',
-                { colSpan: 2, text: 'CWA 311 SPCC' },
                 ''
               ],
               [ 
-                { colSpan: 2, text: 'Regulatory Program(s)' },
+                { colSpan: 1, text: 'Regulatory Program(s)' },
+                { colSpan: 3, text: 'Underground Injection Control ­ Brine' },
                 '',
-                { colSpan: 2, text: 'Underground Injection Control ­ Brine' },
                 ''
               ],
               [ 
-                { colSpan: 2, text: 'Company Name:' },
+                { colSpan: 1, text: 'Company Name:' },
+                { colSpan: 3, text: 'Calgary Energy LLC' },
                 '',
-                { colSpan: 2, text: 'Calgary Energy LLC' },
                 ''
               ],
               [
-                { colSpan: 2, text: 'Facility Name:' },
+                { colSpan: 1, text: 'Facility Name:' },
+                { colSpan: 3, text: 'House Lease' },
                 '',
-                { colSpan: 2, text: 'House Lease' },
                 ''
               ],
               [ 
-                { colSpan: 2, text: 'Facility Physical Location:' },
+                { colSpan: 1, text: 'Facility Physical Location:' },
+                { colSpan: 3, text: 'SW/4 Section 19­T29N­R9E' },
                 '',
-                { colSpan: 2, text: 'SW/4 Section 19­T29N­R9E' },
                 ''
               ],
               [ 
-                { colSpan: 2, text: '(city, state, zip code)' }, 
+                { colSpan: 1, text: '(city, state, zip code)' }, 
+                { colSpan: 3, text: 'Chautauqua, KS' },
                 '',
-                { colSpan: 2, text: 'Chautauqua, KS' },
                 ''
               ],
               [ 
-                { colSpan: 2, text: 'Mailing Address:' },
+                { colSpan: 1, text: 'Mailing Address:' },
+                { colSpan: 3, text: 'P.O. Box 737' },
                 '',
-                { colSpan: 2, text: 'P.O. Box 737' },
                 ''
               ],
               [ 
-                { colSpan: 2, text: '(city, state, zip code)' }, 
+                { colSpan: 1, text: '(city, state, zip code)' }, 
+                { colSpan: 3, text: 'Collinsville, OK 74021' },
                 '',
-                { colSpan: 2, text: 'Collinsville, OK 74021' },
                 ''
               ],           
               [
-                { colSpan: 2, text: 'County/Parish:' },
-                '', 
-                { colSpan: 2, text: 'Osage County' },
+                { colSpan: 1, text: 'County/Parish:' },
+                { colSpan: 3, text: 'Osage County' },
+                '',
                 ''
               ],
               [ 
-                { colSpan: 2, rowSpan: 2, text: 'Facility Contact' },
-                '',
-                { colSpan: 2, text: 'Gary Lewis II (918)798­4511' },
-                '',
-              ],
-              [ 
-                { colSpan: 2, text: '' }, 
-                '',
+                { colSpan: 1, rowSpan: 2, text: 'Facility Contact' },
+                { colSpan: 1, text: 'N/A' },
                 { colSpan: 2, text: 'N/A' },
                 ''
               ],
               [ 
-                { colSpan: 2, text: 'FRS Number:' },
-                '', 
-                { colSpan: 2, text: 'N/A' },
+                '',
+                { colSpan: 3, text: 'N/A' },
+                '',
                 ''
               ],
               [ 
-                { colSpan: 2, text: 'Identification/Permit Number:' }, 
-                '',
-                { colSpan: 2, text: 'N/A' },
-                ''
+                { text: 'Facility Representatives:', rowSpan: 4 },
+                { text: 'N/A' },
+                { text: 'N/A' },
+                { text: 'N/A' }              
               ],
               [ 
-                { colSpan: 2, text: 'Media Number:' }, 
                 '',
-                { colSpan: 2, text: 'N/A' },
-                ''
+                { text: 'N/A' },
+                { text: 'N/A' },
+                { text: 'N/A' }              
               ],
               [ 
-                { colSpan: 2, text: 'NAICS:' }, 
                 '',
-                { colSpan: 2, text: '21111' },
-                ''
+                { text: 'N/A' },
+                { text: 'N/A' },
+                { text: 'N/A' }              
               ],
               [ 
-                { colSpan: 2, text: 'SIC:' }, 
                 '',
-                { colSpan: 2, text: '1311' },
-                ''
+                { text: 'N/A' },
+                { text: 'N/A' },
+                { text: 'N/A' }              
               ],
               [ 
-                { colSpan: 4, rowSpan: 1, text: 'Personnel participating in the inspection:' },
-                '',
-                '',
-                '' 
-              ],
-              [ 'Kent Sanborn', '6EN­WR', 'EPA Inspector', '918-557-1615' ],
-              [ 'N/A', 'N/A', 'N/A', '9187984511' ],
-              [ 'N/A', 'N/A', 'N/A', 'N/A' ],
-              [ 'N/A', 'N/A', 'N/A', 'N/A' ],
-              [ 'N/A', 'N/A', 'N/A', 'N/A' ],
-              [ 'N/A', 'N/A', 'N/A', 'N/A' ],
-              [ 'N/A', 'N/A', 'N/A', 'N/A' ],
-              [ 'N/A', 'N/A', 'N/A', 'N/A' ],
-              [ 'N/A', 'N/A', 'N/A', 'N/A' ],
-              [ 'N/A', 'N/A', 'N/A', 'N/A' ],
-              [ 'N/A', 'N/A', 'N/A', 'N/A' ],
-              [ 'N/A', 'N/A', 'N/A', 'N/A' ],
-              [ 
-                { colSpan: 1, text: 'EPA Lead Inspector \n Signature/Date' }, 
-                { colSpan: 2, text: 'Kent Sanborn' },
-                '',
-                { colSpan: 1, text: 'Date' }
+                { text: 'EPA Inspectors:', rowSpan: 3 },
+                { text: 'Kent Sanborn' },
+                { text: '6EN-WR' },
+                { text: '918-557-1615' }              
               ],
               [ 
-                { colSpan: 1, text: 'Supervisor Signature/Date' }, 
-                { colSpan: 2, text: 'WILLIE LANE' },
                 '',
-                { colSpan: 1, text: 'Date' }
-              ]                   
+                { text: 'N/A' },
+                { text: 'N/A' },
+                { text: 'N/A' }              
+              ],
+              [ 
+                '',
+                { text: 'N/A' },
+                { text: 'N/A' },
+                { text: 'N/A' }              
+              ],
+              [ 
+                { text: 'State Inspector(s):', rowSpan: 2 },
+                { text: 'N/A' },
+                { text: 'N/A' },
+                { text: 'N/A' }              
+              ],
+              [ 
+                '',
+                { text: 'N/A' },
+                { text: 'N/A' },
+                { text: 'N/A' }              
+              ],
+              [ 
+                { text: 'Other Inspector(s):', rowSpan: 2 },
+                { text: 'N/A' },
+                { text: 'N/A' },
+                { text: 'N/A' }              
+              ],
+              [ 
+                '',
+                { text: 'N/A' },
+                { text: 'N/A' },
+                { text: 'N/A' }              
+              ]              
             ]
         }
 
         var docDefinition = {
           header: [
             { text: 'Region 6 Compliance Assurance and Enforcement Division', fontSize: 8, bold: true,  alignment: 'center' },
-            { text: 'INSPECTION REPORT', fontSize: 15, bold: true, alignment: 'center', margin: [ 0, 10, 0, 20 ] },
+            { text: 'INSPECTION REPORT', fontSize: 15, bold: true, alignment: 'center', margin: [ 0, 10, 0, 20 ] }
           ],
 
           footer: {
@@ -156,44 +161,14 @@ $(function () {
             ]
           },
 
-          content: [{
-            table: tbl
-          }]
+          content: [
+            { table: tbl, margin: [ 0, 30, 0, 20 ], fontSize: 8 }
+          ]
 
         };
 
-        pdfMake.createPdf(docDefinition).download();
+        pdfMake.createPdf(docDefinition).download('Public Inspection Report_Access.pdf');
 
     })
 
 })
-
-function htmlToPdfMake(elementID) {
-    var fullText = "";
-    //var x = document.getElementById('htmlContent').elements;
-    //var x = $("a").parent(elementID)
-    var x = $(elementID).children();
-    $.each(x, function(index, value) {
-       //console.log(value);
-        fullText+=$(this).context.innerText + ",";
-        console.log($(this).context.innerText);
-    });
-    
-    var dd = {
-  content: [
-    fullText
-  ]
-    };
-        pdfMake.createPdf(dd).download('optionalName.pdf');
-        console.log(dd.content);
-    
-    //console.log(x);
-    
-    //var html = $(elementID).html();
-    
-    //$("#result").text(html);
-    
-    //var p = $(elementID).html();
-    
-    //$("#resultPDF").text(p);
-}
